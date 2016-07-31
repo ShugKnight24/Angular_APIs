8515665a3e69ed92dcf9b07831801603cda4a6f4
Shugmi Shumunov <SShumunov@gmail.com>
2016-07-31 14:02:27 -0400

    Add footer, add basic styling to cat and dog partials.

---
+++ js/directives/dogdirectives.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var app = angular.module('apiApp');
+
+app.directive("dogeGifs", function() {
+  return {
+    restrict: "AE",
+    templateUrl: "partials/dog.html"
+    replace: false,
+  };
+});
