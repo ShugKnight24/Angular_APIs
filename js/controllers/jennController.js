c8799ce76e3885b399e5d92b87c5a4d80ab91f5f
Shugmi Shumunov <SShumunov@gmail.com>
2016-08-01 10:02:00 -0400

    Resolve Merge Conflict

--- js/controllers/jennController.js
+++ js/controllers/jennController.js
@@ -4,21 +4,9 @@ var app = angular.module('apiApp');
 
 app.controller('jennController',["$scope","jennBadges", function($scope, jennBadges){
   jennBadges.data().then(function(response){
-
-
     $scope.badges = response.data.badges;
-
     // $scope.badges.forEach(function(item, index){
     //   $scope.earned_date = $scope.badges[index].earned_date.slice(0, 9);
-
+    // });
     });
-
-
-
-
-
-
-    });
-
-
 }]);
